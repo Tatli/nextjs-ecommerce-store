7557591644f8d704c2b1e4b127ffffdd5e988047
Osman Tatli <tatli.osman@gmx.at>
2023-10-12 17:41:42 +0200

    Change product prices in migrations and add migrations down workaround

--- migrations/00001-insertIntoProducts.ts
+++ migrations/00001-insertIntoProducts.ts
@@ -4,27 +4,27 @@ const products = [
   {
     id: 1,
     name: 'Website',
-    price: 499.99,
+    price: 499,
     description: 'We create your Websites wireframe and hand it to you',
   },
   {
     id: 2,
     name: 'Website incl Text',
-    price: 999.99,
+    price: 999,
     description:
       'We create your Website and write the Website text for you, based on your input',
   },
   {
     id: 3,
     name: 'Website incl Text and SEO',
-    price: 1999.99,
+    price: 1999,
     description:
       'We create your Website, the text, and Search Machine Optimization',
   },
   {
     id: 4,
     name: '99GB Webspace (monthly)',
-    price: 39.99,
+    price: 39,
     description: 'Your Website will be hosted on our shared host server',
   },
 ];
@@ -46,3 +46,11 @@ export async function down(sql: Sql) {
   `;
   }
 }
+
+// INSERT INTO products
+// (name, price, description)
+// VALUES
+// ('Website', 499, 'We create your Websites wireframe and hand it to you'),
+// ('Website incl Text', 999, 'We create your Website and write the Website text for you, based on your input'),
+// ('Website incl Text and SEO', 1999, 'We create your Website, the text, and Search Machine Optimization'),
+// ('99GB Webspace (monthly)', 39, 'Your Website will be hosted on our shared host server')
